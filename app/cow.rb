94a790c9a72f0e509b8b872319dfb9dc02fa8371
zachariah-spencer <spencerzachariah@gmail.com>
2025-11-04 18:57:03 -0600

    Build lerp click to move code

--- app/cow.rb
+++ app/cow.rb
@@ -8,9 +8,9 @@ class Cow
 
   def initialize
     @position = { x: 400, y: 400 }
+    @velocity = { x: 0, y: 0 }
     @scale = { w: 64, h: 32 }
     @path = "sprites/cow_idle_16x8_2.png"
-
     @animations = {
       idle: {
         path: "sprites/cow_idle_16x8_2.png",
@@ -23,14 +23,42 @@ class Cow
         hold_for: 30
       }
     }
-
     @animation_frame = 0
-    
+    @target_position = @position
+
     enable_tick!
   end
 
   def tick
-    puts "ticking on cow"
+    calc_input
+    @position = Geometry.vec2_add @position, @velocity
+  end
+
+  def calc_input
+    @target_position = { x: inputs.mouse.x, y: inputs.mouse.y } if inputs.mouse.click
+    # outputs.debug << "TARGET POSITION: #{@target_position}"
+    # outputs.debug << "POSITION: #{@position}"
+    # outputs.debug << "IS POSITIONS APPROX EQUAL? #{approximately_equal? @position.values, @target_position.values}"
+    if approximately_equal? @position.values, @target_position.values
+      @velocity = { x: 0, y: 0 }
+      return
+    end
+    
+    line_to_target = Geometry.vec2_sub(@target_position, @position)
+    @velocity = Geometry.vec2_scale line_to_target, 0.08
+    @velocity.transform_values! { |v| v.clamp(-5.0, 5.0) }
+  end
+
+
+
+  def approximately_equal?(a, b, tolerance: 0.5)
+    # Check if arrays have the same length
+    return false if a.length != b.length
+
+    a.zip(b).all? do |val_a, val_b|
+      # Calculate the tolerance condition for each element pair
+      (val_b - val_a).abs <= tolerance
+    end
   end
 
   def rect
